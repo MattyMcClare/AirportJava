1f7f29e8120e69bc6ae52157f20d9a58d0113eb6
Mateusz <matstanczak@gmail.com>
2019-05-10 18:08:22 +0100

    flight test passing

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,24 @@
 public class Flight {
+
+    private String plane;
+    private int flightNumber;
+    private String destination;
+
+    public Flight(String plane, int flightNumber, String destination) {
+        this.plane = plane;
+        this.flightNumber = flightNumber;
+        this.destination = destination;
+    }
+
+    public String getPlane() {
+        return plane;
+    }
+
+    public int getFlightNumber() {
+        return flightNumber;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
 }
--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,2 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class FlightTest {
+
+    Flight flight;
+
+    @Before
+    public void before(){
+        flight = new Flight("AirBus320", 548, "GLA");
+    }
+
+    @Test
+    public void hasPlane(){
+        assertEquals("AirBus320", flight.getPlane());
+    }
+    
+    @Test
+    public void hasFlightNumber(){
+        assertEquals(548, flight.getFlightNumber());
+    }
+
+    @Test
+    public void hasDestination(){
+        assertEquals("GLA", flight.getDestination());
+    }
 }
